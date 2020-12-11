88bbf63586d5509de080b22eaf3bee44f0a540b2
Soren Thompson <sorenct04@gmail.com>
2020-12-11 11:41:54 -0700

    add .gitignore and untrack nm and cache

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"Gain Inspiraton","short_name":"Gain Inspiraton","start_url":"/","background_color":"#fff","theme_color":"#4299e1","display":"standalone","icon":"src/images/icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"93eadc9fc24b4f0ee6980ccff5a33464"},
-    }]
